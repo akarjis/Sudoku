9f0aa35ce6545f652600d8ad91a83579b70cc251
somename <someemail>
2015-09-30 15:26:28 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
@@ -8,7 +8,9 @@ import org.junit.Test;
 
 public class TestSudokuVerifier {
 
-	SudokuVerifier verifier;
+	private SudokuVerifier verifier;
+	private String correctString = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";
+	private String incorrectString = "123456789912345678891234567789123456678912345567891234456789123345678912234567891";
 
 	@Before
 	public void setUp() {
@@ -45,6 +47,11 @@ public class TestSudokuVerifier {
 		String zeroValuesString = "017369825632158947958724316825437169791586432346912758289643571573291684164875293";
 		ret = verifier.verify(zeroValuesString);
 		assertEquals(-1, ret);
+	}
+	
+	@Test
+	public void testRows() {
+		String correct
 	}
 
 }
