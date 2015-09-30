1d4ddb720bcca9bffc59ff33cc31c300b8de9be5
somename <someemail>
2015-09-30 15:16:06 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
@@ -27,15 +27,23 @@ public class TestSudokuVerifier {
 	}
 	
 	@Test
-	public void testVerifyCorrectStringFormat() {
+	public void testVerifyCorrectStringLength() {
 		String tooShortString = "41736982563215894795872431682543716979158643234691275828964357157329168416487529";
 		int ret = verifier.verify(tooShortString);
 		assertEquals(-1, ret);
 		String tooLongString = "41736982563215894795872431682543716979158643234691275828964357157329168416487529234234";
 		ret = verifier.verify(tooLongString);
 		assertEquals(-1, ret);
+	}
+	
+	@Test void testVerifyNoNegativeOrZeroValues() {
+		String negativeValuesString = "-4173698256321589479587243168254371697915864323469127582896435715732916841648752923";
+		int ret = verifier.verify(negativeValuesString);
+		assertEquals(-1, ret);
 		
-		
+		String zeroValuesString = "0173698256321589479587243168254371697915864323469127582896435715732916841648752923";
+		ret = verifier.verify(zeroValuesString);
+		assertEquals(-1, ret);
 	}
 
 }
