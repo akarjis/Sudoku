db4d5dafe299790ef69dd4543cd0eb2178946e14
somename <someemail>
2015-09-30 14:59:09 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
@@ -2,13 +2,29 @@ package org.univoulu.tol.sqatlab.sudoku;
 
 import static org.junit.Assert.*;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 public class TestSudokuVerifier {
 
+	SudokuVerifier verifier;
+
+	@Before
+	public void setUp() {
+		verifier = new SudokuVerifier();
+	}
+	
+	@After
+	public void tearDown() {
+		verifier = null;
+	}
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testVerifyFailEmptyString() {
+		int ret = verifier.verify("");
+		assertEquals(1, ret);
+		
 	}
 
 }
